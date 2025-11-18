56027baac61f897ee9a5a7dd394267a04c8262b5
Nikhil Padala <nikhilpadala99@gmail.com>
2025-11-18 13:16:36 +0530

    push project

---
+++ projectsql/2.top_paying_jobskills.sql
@@ -0,0 +1,36 @@
+
+/*
+-- 2.Question :: what are the top  skills required for data analyst jobs
+           who works remotely ??
+   
+   */
+with top_paying_job as (
+select 
+    job_id,
+    job_title,
+    salary_year_avg,
+    job_country,
+  
+    company_dim.name as company_name
+from 
+    job_postings_fact 
+left join company_dim on job_postings_fact.company_id=company_dim.company_id
+where job_title_short='Data Analyst' and
+        job_work_from_home=true  AND
+        salary_year_avg IS NOT NULl
+order by 
+        salary_year_avg desc
+limit 10
+)
+select 
+       top_paying_job.*,
+       skills_dim.skills
+
+from top_paying_job
+inner join  skills_job_dim on skills_job_dim.job_id=top_paying_job.job_id
+inner join  skills_dim on skills_dim.skill_id=skills_job_dim.skill_id 
+order by 
+       salary_year_avg desc
+       limit 10;
+
+
---
+++ projectsql/3.top_demading_Dataanalystskills.sql
@@ -0,0 +1,29 @@
+/* 3.Question :: Top Demanding Skills for Data Analyst ??
+               and average salary for that skills*/
+
+with skill_count_tab as (
+select 
+    round(avg(salary_year_avg),2)as avg_sal_USD,
+     skill_id ,
+     count(*) as skill_count
+   
+from 
+   skills_job_dim
+Inner join job_postings_fact on job_postings_fact.job_id=skills_job_dim.job_id
+where 
+      job_title_short='Data Analyst' and
+      salary_year_avg is not null
+group by skill_id
+)
+select 
+      
+       skill_count_tab.skill_id,
+       skills,
+       skill_count_tab.skill_count,
+       skill_count_tab.avg_sal_USD
+     
+from 
+    skills_dim 
+Inner JOIN skill_count_tab on skill_count_tab.skill_id=skills_dim.skill_id
+order by skill_count desc
+limit 10;
